d98a63ef9026220ae26e0ba1094d52f29816265f
alex <alex@alex-PC>
2017-06-08 15:49:52 -0300

    sdsdsd

--- src/main/java/org/jboss/tools/example/projeto/listener/ContextLoaderListenerImpl.java
+++ src/main/java/org/jboss/tools/example/projeto/listener/ContextLoaderListenerImpl.java
@@ -2,25 +2,23 @@ package org.jboss.tools.example.projeto.listener;
 
 import java.io.Serializable;
 
-import javax.faces.bean.ApplicationScoped;
-
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-@ApplicationScoped
-public class ContextLoaderListenerImpl extends ContextLoaderListener implements Serializable{
+public class ContextLoaderListenerImpl extends ContextLoaderListener implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	private static WebApplicationContext getWac(){
-		return WebApplicationContextUtils.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());
+
+	private static WebApplicationContext getWac() {
+		return WebApplicationContextUtils
+				.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());
 	}
-	
+
 	public static Object getBean(String idNomeBean) {
 		return getWac().getBean(idNomeBean);
 	}
-	
+
 	public static Object getBean(Class<?> classe) {
 		return getWac().getBean(classe);
 	}
