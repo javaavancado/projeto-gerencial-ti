347b8f307cfb6336878680f3febfdc353397878a
alex <alex@alex-PC>
2017-07-04 11:27:19 -0300

    teste

--- src/main/java/org/jboss/tools/example/jsf/managedbean/UserManageBean.java
+++ src/main/java/org/jboss/tools/example/jsf/managedbean/UserManageBean.java
@@ -1,11 +1,14 @@
 package org.jboss.tools.example.jsf.managedbean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import org.jboss.tools.example.springmvc.data.MemberDao;
 import org.jboss.tools.example.springmvc.model.Member;
+import org.primefaces.event.FileUploadEvent;
 
 @RequestScoped
 @ManagedBean(name = "userManageBean")
@@ -21,6 +24,12 @@ public class UserManageBean {
 		member = new Member();
 		return "/cadastro/member.jsf";
 	}
+	
+	
+	 public void handleFileUpload(FileUploadEvent event) {
+	        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+	        FacesContext.getCurrentInstance().addMessage(null, message);
+	    }
 
 	public void setMemberDao(MemberDao memberDao) {
 		this.memberDao = memberDao;
