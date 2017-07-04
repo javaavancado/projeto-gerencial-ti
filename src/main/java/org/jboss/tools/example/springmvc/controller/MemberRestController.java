adeff1a1455dc8732c6fbbb297c0fe00daac0f2e
alex <alex@alex-PC>
2017-07-04 18:56:29 -0300

    correção do rest ful de usuários

--- src/main/java/org/jboss/tools/example/springmvc/controller/MemberRestController.java
+++ src/main/java/org/jboss/tools/example/springmvc/controller/MemberRestController.java
@@ -20,9 +20,9 @@ import java.util.List;
 
 import org.jboss.tools.example.springmvc.data.MemberDao;
 import org.jboss.tools.example.springmvc.model.Member;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,7 +34,7 @@ public class MemberRestController {
     @Autowired
     private MemberDao memberDao;
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(produces = {"application/json"})
     public @ResponseBody List<Member> listAllMembers() {
         return memberDao.findAllOrderedByName();
     }
